51501837eb0d1a01ed8311274733153156743ff3
mickplayer <mickgamehome@gmail.com>
2022-11-25 06:00:39 +0800

    Change package nane

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use card_game::{Deck, Game, SystemCall};
+use card_battle_game::{Game, SystemCall};
 use read_input::shortcut::input;
 
 fn main() {
@@ -12,24 +12,5 @@ fn main() {
             SystemCall::Next => (),
             SystemCall::Stop => break,
         }
-    }
-
-    let mut deck = Deck::new();
-
-    fn draw_from_deck(deck: &mut Deck) {
-        let card = deck.draw();
-        let card = match card {
-            Some(card) => card,
-            None => {
-                println!("Shuffle the full deck.");
-                deck.full_shuffle();
-                deck.draw().unwrap()
-            }
-        };
-        println!("Draw a card: {}", card);
-    }
-
-    for _ in 0..60 {
-        draw_from_deck(&mut deck);
     }
 }
